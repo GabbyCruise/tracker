f0b347254faa80fc982a93c79933b4c2cc3a64b8
Gabriel Jonah <36955901+GabbyCruise@users.noreply.github.com>
2022-05-31 22:53:35 +0100

    decoupling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import todos from './components/todos.vue'
+  export default {
+    name: 'App',
+    components: {
+      todos
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 10px
+  }
 </style>
---
+++ src/components/todos.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    {{ name }}
+
+    <!-- {{ btnState ? 'the button is disabled' : 'The button is active' }} -->
+    <!-- <button v-on:click="changeName" v-bind:disabled="btnState"> Change Name </button> -->
+
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in todo" :key='index'>{{ index }}. {{ data.action }}</li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-list',
+    data(){
+      return {
+
+        todo: [
+          {'action': 'Watch vue tutorial'}, 
+          {'action':'Follow tutorial coding samples'}
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
